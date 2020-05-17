173076c6abd11f361285057837be6c48e1398fcd
probabilityfilter <arunkumar@asu.edu>
2020-05-17 13:38:07 -0400

    straight line

--- straight_line/simple_streamlit_app.py
+++ straight_line/simple_streamlit_app.py
@@ -5,17 +5,29 @@ run the app by installing streamlit with pip and typing
 > streamlit run simple_streamlit_app.py
 """
 
+import numpy as np
 import streamlit as st
+from bokeh.plotting import figure
 
-st.title('Simple Streamlit App')
+st.title('Simple Classification')
 
-x = st.slider('x')
-st.write(x, 'squared is', x * x)
+slope = st.slider('Slope')
+intercept = st.slider('Intercept')
 
-s = st.text_input('Type a name in the box below')
-st.write(f'Hello {s}')
+x = np.arange(-3.0, 3.0, 0.1)
+y = slope * x + intercept
 
-if st.button('Say hello'):
-     st.write('Why hello there')
-else:
-     st.write('Goodbye')
+p = figure(
+    title="Mohr's Circle",
+    x_axis_label="stress",
+    y_axis_label="shear",
+    match_aspect=True,
+    tools="pan,reset,save,wheel_zoom",
+)
+
+p.line(x, y, color="#ff7f0e", line_width=3, line_alpha=0.6)
+
+p.xaxis.fixed_location = 0
+p.yaxis.fixed_location = 0
+
+st.bokeh_chart(p)
